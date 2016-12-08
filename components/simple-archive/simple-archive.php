f270e7b9555bc25ee1794794487ca1bead5cb584
ghostdevelop <ghost46@protonmail.com>
2016-12-07 21:22:45 -0500

    simple archive thumbnail size

--- components/simple-archive/simple-archive.php
+++ components/simple-archive/simple-archive.php
@@ -1,13 +1,14 @@
 <?php $SimpleArchive = new SimpleArchive($options)?>
 <div id="<?php echo $SimpleArchive->id ?>" class="<?php echo $SimpleArchive->type ?>">
 	<?php $news = new WP_Query(array($options['query']));?>
+	<?php $img_size = (isset($options['img_size']) ? $options['img_size'] : 'thumbnail')?>
 	<?php if (isset($options['title'])):?><h2><?php echo $options['title']?></h2><?php endif;?>
 	<?php if ($news->have_posts()):?>
 		<?php while ($news->have_posts()): $news->the_post()?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<?php if (has_post_thumbnail()):?>
 					<div class="archive-thumbnail">
-						<?php the_post_thumbnail('thumbnail')?>
+						<?php the_post_thumbnail($img_size)?>
 					</div>
 				<?php endif;?>
 				<div class="archive-content">
